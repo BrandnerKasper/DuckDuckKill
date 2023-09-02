a6391ab20941eb32391378bad2455077e82efa65
Andreas Braun <brauney95@gmail.com>
2023-09-02 16:54:33 +0200

    screenshake a bit less shitty

--- scripts/JUICE_ScreenShake.cs
+++ scripts/JUICE_ScreenShake.cs
@@ -4,60 +4,57 @@ using System;
 public partial class JUICE_ScreenShake : Node
 {
     private Camera2D _camera;  // The camera you wanna shake
-    private double _shakeAmount = 0.0f; // How much to shake
-    private double _shakeDuration = 0.0f; // How long to shake for
-    private Vector2 _originalPosition; // Store the original camera position
-    private Vector2 _currentOffset;
+    private float _shakeAmount = 0.0f; // How much to shake
+    private float _shakeDuration = 0.0f; // How long to shake for
+    private float _initialShakeDuration = 0.0f; // How long to shake for
+    private Vector2 _originalOffset; // Store the original camera offset
     private bool _isShaking = false;
 
     public override void _Ready()
     {
         _camera = GetParent().GetNode<Camera2D>("Camera2D");
-        _originalPosition = _camera.Position;
     }
-    
+
     public override void _Process(double delta)
     {
-        
-        // Update the shake effect
-        if (_shakeDuration > 0)
+        if (_shakeDuration != 0)
         {
-            if(!_isShaking)
+            // set initial max offset
+            if (!_isShaking)
             {
-                _isShaking = true;
-                _currentOffset = new Vector2((float)_shakeAmount, (float)_shakeAmount);
-                _originalPosition = _camera.Position;
+                _initialShakeDuration = _shakeDuration;
+                // Randomize the offset within the shake amount
+                float xOffset = (float)GD.RandRange(-_shakeAmount, _shakeAmount);
+                float yOffset = (float)GD.RandRange(-_shakeAmount, _shakeAmount);
+                _camera.Offset = _originalOffset + new Vector2(xOffset, yOffset);
             }
-            
-            GD.Print("shake");
-            // Randomize the camera's position a bit
-            Vector2 shake = new Vector2((float)GD.RandRange(-_shakeAmount, _shakeAmount), (float)GD.RandRange(-_shakeAmount, _shakeAmount));
-            _camera.Position = _originalPosition + shake;
 
-            // Decrease the shake duration and amount
+            // Decrease max Offset by tweening down to 0
             _shakeDuration -= (float)delta;
-            _shakeAmount = (float)Mathf.Lerp(_shakeAmount, 0, 1 - _shakeDuration / 0.5f); // Notice the parameter change here
-
-        }
-        else
-        {
-            _isShaking = false;
-            _currentOffset = new Vector2(0, 0);
-            _shakeAmount = 0.0f;
-            _shakeDuration = 0.0f;
-            _camera.Position = _originalPosition;
+            _shakeAmount = Mathf.Lerp(_shakeAmount, 0, _initialShakeDuration-_shakeDuration);
+            if(_isShaking)
+                _isShaking = false;
+            else
+                _camera.Offset = _originalOffset + new Vector2(_shakeAmount, _shakeAmount);
             
-            _camera.PositionSmoothingEnabled = true;
+            float xOffset2 = (float)GD.RandRange(-_shakeAmount, _shakeAmount);
+            float yOffset2 = (float)GD.RandRange(-_shakeAmount, _shakeAmount);
+            _camera.Offset = _originalOffset + new Vector2(xOffset2, yOffset2);
+            
+            GD.Print(_shakeAmount);
+            
+            if (_shakeDuration <= 0)
+            {
+                _isShaking = false;
+                _shakeDuration = 0;
+                _camera.Offset = _originalOffset; // Reset to original position
+            }
         }
     }
 
-    public  void StartShake(float amount, float duration)
+    public void StartShake(float amount, float duration)
     {
         _shakeAmount = amount;
         _shakeDuration = duration;
-        _currentOffset = new Vector2(amount, amount );
-        
-        _camera.PositionSmoothingEnabled = false;
-        //camera.Position = desiredPos;
     }
 }
