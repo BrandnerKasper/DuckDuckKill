ba3e8582f6da9d72fd60eee5ba1b7ca756b43abe
BrandnerKasper <tobias.brandner@stud-mail.uni-wuerzburg.de>
2023-09-02 10:40:16 +0200

    init project

---
+++ scripts/Player.cs
@@ -0,0 +1,41 @@
+using Godot;
+
+namespace DuckDuckKill.scripts;
+
+public partial class Player : CharacterBody2D
+{
+	public const float Speed = 300.0f;
+	public const float JumpVelocity = -400.0f;
+
+	// Get the gravity from the project settings to be synced with RigidBody nodes.
+	public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
+
+	public override void _PhysicsProcess(double delta)
+	{
+		GD.Print(Engine.GetFramesPerSecond());
+		Vector2 velocity = Velocity;
+
+		// Add the gravity.
+		if (!IsOnFloor())
+			velocity.Y += gravity * (float)delta;
+
+		// Handle Jump.
+		if (Input.IsActionJustPressed("jump") && IsOnFloor())
+			velocity.Y = JumpVelocity;
+
+		// Get the input direction and handle the movement/deceleration.
+		// As good practice, you should replace UI actions with custom gameplay actions.
+		Vector2 direction = Input.GetVector("left", "right", "down", "up");
+		if (direction != Vector2.Zero)
+		{
+			velocity.X = (float)(direction.X * Speed);
+		}
+		else
+		{
+			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+		}
+
+		Velocity = velocity;
+		MoveAndSlide();
+	}
+}
