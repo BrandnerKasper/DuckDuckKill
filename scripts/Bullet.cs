e883be684c042e08cfc235430a77ae171c5939e7
Andreas Braun <brauney95@gmail.com>
2023-09-03 15:53:06 +0200

    egg animation!!!

--- scripts/Bullet.cs
+++ scripts/Bullet.cs
@@ -3,57 +3,66 @@ using Godot;
 
 public partial class Bullet : CharacterBody2D
 {
-    private float _lifeTime = 2.0f;
-    private int _speed = 500;
-    private Vector2 _initialVelocity = new(0, 0);
-    private float _initialScale;
+	private float _lifeTime = 2.0f;
+	private int _speed = 500;
+	private Vector2 _initialVelocity = new(0, 0);
+	private float _initialScale;
+	private AnimatedSprite2D _animatedSprite2D;
 
-    public override void _Process(double delta)
-    {
-        _lifeTime -= (float)delta;
-        if (_lifeTime <= 0)
-        {
-            GetParent().QueueFree();
-        }
+	public override void _Ready()
+	{
+		_animatedSprite2D = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+		_animatedSprite2D.AnimationFinished += () => QueueFree();
+	}
 
-        var speedScale = _initialScale * Velocity.Length() / _initialVelocity.Length();
-        Scale = new Vector2(speedScale, speedScale);
-    }
+	public override void _Process(double delta)
+	{
+		_lifeTime -= (float)delta;
+		if (_lifeTime <= 0)
+		{
+			GetParent().QueueFree();
+		}
 
-    public override void _PhysicsProcess(double delta)  // Sticking with float for Godot's usual _PhysicsProcess
-    {
-        // Define falloff rate per second
-        float falloffRate = 0.01f; 
+		var speedScale = _initialScale * Velocity.Length() / _initialVelocity.Length();
+		Scale = new Vector2(speedScale, speedScale);
+	}
 
-        // Compute the falloff factor for this frame
-        float falloffFactor = (float)Math.Exp(-falloffRate * delta);
+	public override void _PhysicsProcess(double delta)  // Sticking with float for Godot's usual _PhysicsProcess
+	{
+		// Define falloff rate per second
+		float falloffRate = 0.01f; 
+
+		// Compute the falloff factor for this frame
+		float falloffFactor = (float)Math.Exp(-falloffRate * delta);
 		
-        Velocity *= falloffFactor;
+		Velocity *= falloffFactor;
 
-        var collision = MoveAndCollide(Velocity * (float)delta);
-        if (collision != null)
-        {
-            Velocity = Velocity.Bounce(collision.GetNormal()) * 0.9f;
-        }
-    }
+		var collision = MoveAndCollide(Velocity * (float)delta);
+		if (collision != null)
+		{
+			Velocity = Velocity.Bounce(collision.GetNormal()) * 0.9f;
+			_animatedSprite2D.Play("burst");
 
-    public void Start(float Rotation)
-    {
-        //GlobalPosition = Pos;
-        this.Rotation = Rotation;
-        // move in the direction of the _rotation
-        Vector2 velocity = new Vector2(0, 0);
-        velocity.X = Mathf.Cos(Rotation);
-        velocity.Y = Mathf.Sin(Rotation);
-        _initialVelocity = Velocity = velocity.Normalized() * _speed;
-        _initialScale = Scale.X;
-    }
+		}
+	}
+
+	public void Start(float Rotation)
+	{
+		//GlobalPosition = Pos;
+		this.Rotation = Rotation;
+		// move in the direction of the _rotation
+		Vector2 velocity = new Vector2(0, 0);
+		velocity.X = Mathf.Cos(Rotation);
+		velocity.Y = Mathf.Sin(Rotation);
+		_initialVelocity = Velocity = velocity.Normalized() * _speed;
+		_initialScale = Scale.X;
+	}
 
 
-    private void OnVisibilityNotifier2DScreenExited()
-    {
-        GD.Print("Bullet exited the screen");
-        // Deletes the bullet when it exits the screen.
-        QueueFree();
-    }
+	private void OnVisibilityNotifier2DScreenExited()
+	{
+		GD.Print("Bullet exited the screen");
+		// Deletes the bullet when it exits the screen.
+		QueueFree();
+	}
 }
